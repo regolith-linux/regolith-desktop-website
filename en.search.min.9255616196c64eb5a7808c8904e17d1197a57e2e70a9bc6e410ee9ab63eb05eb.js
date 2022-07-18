197bcdc92466463ac02cc11e51e32f31acd07e38
kgilmer <>
2022-07-18 01:38:49 +0000

    kgilmer published a site update

--- en.search.min.9255616196c64eb5a7808c8904e17d1197a57e2e70a9bc6e410ee9ab63eb05eb.js
+++ en.search.min.9255616196c64eb5a7808c8904e17d1197a57e2e70a9bc6e410ee9ab63eb05eb.js
@@ -1,4 +1,4 @@
-'use strict';(function(){const searchDataURL='/en.search-data.min.a1fd95626a0ef4ea816082b98c3ac52f448fb339dbaa3b3637a259945a55cfcb.json';const indexConfig=Object.assign({cache:true},{doc:{id:'id',field:['title','content'],store:['title','href','section']}});const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
+'use strict';(function(){const searchDataURL='/en.search-data.min.12557ee4b1c6d5528fd8b1c8817e40cc092e37105a9044515ef10c32c4f958f1.json';const indexConfig=Object.assign({cache:true},{doc:{id:'id',field:['title','content'],store:['title','href','section']}});const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
 input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(event){if(event.target.value!==undefined){return;}
 if(input===document.activeElement){return;}
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
